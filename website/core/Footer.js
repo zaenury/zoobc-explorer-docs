f76b247d8e3789218a6f21111e884a3bd5759cfa
I Gede Tirtanata <igedetirtanata@gmail.com>
2020-02-20 11:39:24 +0800

    docs-footer (#2)

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -1,29 +1,26 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-const React = require('react');
+const React = require("react");
 
 class Footer extends React.Component {
   docUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
     const docsUrl = this.props.config.docsUrl;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ""}`;
+    const langPart = `${language ? `${language}/` : ""}`;
     return `${baseUrl}${docsPart}${langPart}${doc}`;
   }
 
   pageUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
-    return baseUrl + (language ? `${language}/` : '') + doc;
+    return baseUrl + (language ? `${language}/` : "") + doc;
   }
 
   render() {
     return (
-      <footer className="nav-footer" id="footer">
+      <footer
+        className="nav-footer"
+        id="footer"
+        style={{ background: "#0b3d65 !important" }}
+      >
         <section className="sitemap">
           <a href={this.props.config.baseUrl} className="nav-home">
             {this.props.config.footerIcon && (
@@ -37,82 +34,105 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
+            <a href={this.docUrl("doc1.html", this.props.language)}>
               Getting Started (or other categories)
             </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
+            <a href={this.docUrl("doc2.html", this.props.language)}>
               Guides (or other categories)
             </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
+            <a href={this.docUrl("doc3.html", this.props.language)}>
               API Reference (or other categories)
             </a>
           </div>
           <div>
-            <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              User Showcase
+            <h5>Our Projects</h5>
+            <a
+              href="https://zoobc.com/ZooBC%20Whitepaper%20Draft%20-%20V0.2.pdf"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              ZooBC Whitepaper
             </a>
             <a
-              href="https://stackoverflow.com/questions/tagged/"
+              href="https://zoobc.one/"
               target="_blank"
-              rel="noreferrer noopener">
-              Stack Overflow
+              rel="noreferrer noopener"
+            >
+              ZooBC Wallet
             </a>
-            <a href="https://discordapp.com/">Project Chat</a>
             <a
-              href="https://twitter.com/"
+              href="https://zoobc.net/"
               target="_blank"
-              rel="noreferrer noopener">
-              Twitter
+              rel="noreferrer noopener"
+            >
+              ZooBC Explorer
             </a>
           </div>
           <div>
-            <h5>More</h5>
-            <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
-            <a href="https://github.com/">GitHub</a>
+            <h5>Channels</h5>
             <a
-              className="github-button"
-              href={this.props.config.repoUrl}
-              data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
-              data-show-count="true"
-              data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star this project on GitHub">
-              Star
+              href="https://zoobc.org/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Discussion Forums
             </a>
-            {this.props.config.twitterUsername && (
-              <div className="social">
-                <a
-                  href={`https://twitter.com/${this.props.config.twitterUsername}`}
-                  className="twitter-follow-button">
-                  Follow @{this.props.config.twitterUsername}
-                </a>
-              </div>
-            )}
-            {this.props.config.facebookAppId && (
-              <div className="social">
-                <div
-                  className="fb-like"
-                  data-href={this.props.config.url}
-                  data-colorscheme="dark"
-                  data-layout="standard"
-                  data-share="true"
-                  data-width="225"
-                  data-show-faces="false"
-                />
-              </div>
-            )}
+            <a
+              href="https://t.me/ZooBlockchain"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Community Chat
+            </a>
+          </div>
+          <div>
+            <h5>Social Media</h5>
+            <a
+              href="https://fb.me/TheZooBC"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Facebook
+            </a>
+            <a
+              href="https://twitter.com/TheZooBC"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Twitter
+            </a>
+            <a
+              href="https://linkedin.com/showcase/zoobc"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://www.instagram.com/TheZooBC"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Instagram
+            </a>
+            <a
+              href="http://www.youtube.com/c/BlockchainZoo"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              YouTube
+            </a>
           </div>
         </section>
-
         <a
-          href="https://opensource.facebook.com/"
+          href="https://blockchainzoo.com/"
           target="_blank"
           rel="noreferrer noopener"
-          className="fbOpenSource">
+          className="fbOpenSource"
+        >
           <img
-            src={`${this.props.config.baseUrl}img/oss_logo.png`}
-            alt="Facebook Open Source"
+            src={`${this.props.config.baseUrl}img/bcz_logo_1.png`}
+            alt="Blockchain Zoo"
             width="170"
             height="45"
           />
